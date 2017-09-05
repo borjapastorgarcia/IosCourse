33f0be41b8602425759976e2ae07a6285f2e36ba
Sandra Diez <sandradiez@MacBook-Pro-de-Sandra.local>
2017-09-05 12:25:21 +0200

    Hacer que el teclado se esconda al tocar return

--- HolaMundo/ViewController.m
+++ HolaMundo/ViewController.m
@@ -1,13 +1,12 @@
 //
 //  ViewController.m
 //  HolaMundo
-//
 //  Copyright © 2017 Borja Pastor. All rights reserved.
 //
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *label;
 @property (weak, nonatomic) IBOutlet UITextField *texto;
 @property (weak, nonatomic) IBOutlet UIButton *boton;
@@ -15,15 +14,23 @@
 @end
 
 @implementation ViewController
+-(void)cambiaTexto {
+    self.label.text = self.texto.text;
+}
 - (IBAction)accionHola:(id)sender {
-      [self.label setText:[self.texto text]];
+    [self cambiaTexto];
+}
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self cambiaTexto];
+    [_texto resignFirstResponder];
+    return YES;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
